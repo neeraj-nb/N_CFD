2277401168214a7e5200c9abc7ff8b630685aa96
Neeraj Babu <neerajbabu27@gmail.com>
2024-04-10 11:06:02 +0530

    add step 1

---
+++ 12 steps to Navier-Stokes/step1.py
@@ -0,0 +1,29 @@
+import numpy as np
+from matplotlib import pyplot
+import time, sys
+
+# 1D diffusion problem
+
+# setup
+nx = 41 # number of nodes
+dx = 2/(nx -1) # distance between nodes
+nt = 25 # number time steps
+dt = 0.025 # time step
+c = 1 # wave speed
+
+#initial condition
+u = np.ones(nx) # u=1 everywhere
+u[int(0.5/dx):int(1/dx+1)] = 2 # u=2 for between x 0.5 and 1
+print(u)
+pyplot.plot(np.linspace(0,2,nx),u)
+
+# solving
+un = np.ones(nx)
+for n in range(nt):
+    un = u.copy()
+    for i in range(nx):
+        u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
+
+pyplot.plot(np.linspace(0, 2, nx), u);
+
+pyplot.show()
